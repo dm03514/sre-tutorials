5ee4929db16212d8f4111c260769c9fef0e426e3
dm03514 <dm03514@gmail.com>
2018-11-15 15:57:32 -0500

    Service Skeleton And Metrics!

--- performance/analysis-methodology-simple-http/cmd/server/main.go
+++ performance/analysis-methodology-simple-http/cmd/server/main.go
@@ -3,19 +3,47 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"html"
 	"net/http"
 	"time"
 	"log"
 	"database/sql"
 	"encoding/json"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	_ "github.com/lib/pq"
 )
+
+var (
+	requestLatency = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "http_request_seconds",
+		Help: "Distribution of request lengths",
+	}, []string{"path"})
+
+	findByAgeLatency = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "find_by_age_seconds",
+		Help: "Distribution of find by age durations",
+	})
+
+	findByAgeResultCount = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "find_by_age_results_count",
+		Help: "# of results returned from find by age",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(requestLatency)
+	prometheus.MustRegister(findByAgeLatency)
+	prometheus.MustRegister(findByAgeResultCount)
+}
 
 type Postgres struct {
 	db *sql.DB
 }
 
-type People []Person
+type PeopleResponse struct {
+	People []Person
+}
 
 type Person struct {
 	Address string
@@ -27,16 +55,19 @@ type Payload struct {
 	Age int
 }
 
-func (p Payload) Bytes() ([]byte, error) {
-	return json.Marshal(p)
-}
-
-
 type Handler struct {
 	Postgres *Postgres
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	defer func() {
+		diff := time.Since(start)
+		// fmt.Fprintf(w, "Took: %s\n", diff)
+		requestLatency.WithLabelValues(html.EscapeString(r.URL.Path)).Observe(diff.Seconds())
+	}()
+
 	payload := Payload{}
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
@@ -47,23 +78,30 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	result, err := h.Postgres.FindByAge(payload.Age)
+	people, err := h.Postgres.FindByAge(payload.Age)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	resp := PeopleResponse{
+		People: people,
+	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(&result)
+	json.NewEncoder(w).Encode(&resp)
 }
 
 
-func (p *Postgres) FindByAge(age int) (People, error) {
-	var people People
+func (p *Postgres) FindByAge(age int) ([]Person, error) {
+	start := time.Now()
+	people := []Person{}
 
-	q := `
-SELECT address, full_name, age FROM people WHERE age = ?
-`
+	defer func() {
+		diff := time.Since(start)
+		findByAgeLatency.Observe(diff.Seconds())
+	}()
+
+	q := `SELECT address, full_name, age FROM people WHERE age = ?`
 
 	rows, err := p.db.Query(q, age)
 	if err != nil {
@@ -83,6 +121,8 @@ SELECT address, full_name, age FROM people WHERE age = ?
 	if err := rows.Err(); err != nil {
 		return people, err
 	}
+
+	findByAgeResultCount.Set(float64(len(people)))
 
 	return people, nil
 }
@@ -117,11 +157,13 @@ func main() {
 		Postgres: postgres,
 	}
 
-	http.Handle("/metrics", promhttp.Handler())
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.Handle("/", h)
 
 	s := &http.Server{
 		Addr:           ":8080",
-		Handler:        h,
+		Handler:        mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
--- performance/analysis-methodology-simple-http/config/postgres/schema.sh
+++ performance/analysis-methodology-simple-http/config/postgres/schema.sh
@@ -4,10 +4,10 @@ psql -c "create database people"
 
 psql -c "create schema people"
 
-psql ipsets -c "CREATE TABLE people (
+psql people -c "CREATE TABLE people (
     last_updated_time TIMESTAMP WITH TIME ZONE default current_timestamp,
     address text NOT NULL,
     full_name text NOT NULL,
-    age integer NOT NULL
+    age integer NOT NULL,
     PRIMARY KEY (full_name, address)
 )"
